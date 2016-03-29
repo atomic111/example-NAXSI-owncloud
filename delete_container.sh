521c15980c11e82a1b2762a852d8a0a6e8b7d66d
Patrick Münch <patrick.muench1111@gmail.com>
2016-03-29 17:01:50 +0200

    adjusted start and delete scripts for docker-compose
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- delete_container.sh
+++ delete_container.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#echo "stop container"
-#docker stop owncloud-data owncloud-postgres-data owncloud-postgres owncloud-nginx owncloud-naxsi elasticsearch
-echo "remove container"
-docker rm -f owncloud-data owncloud-postgres-data owncloud-postgres owncloud-nginx owncloud-naxsi elasticsearch
+docker-compose -p owncloud down
--- start.sh
+++ start.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-cd ./data/db
-sh build-and-run.sh
+docker-compose -p owncloud up -d
 
-cd ../owncloud/
-sh build-and-run.sh
-
-cd ../../db/
-sh build-image.sh
-sh create_start.sh
-
-cd ../nginx/
-sh build-image.sh
-sh create_start.sh
-
-cd ../waf/
-sh build-image.sh
-sh create_start.sh
